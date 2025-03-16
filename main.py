3b10f4abc506969d424e1a06d1a1d5ce527a3004
Dell <misha.shulgin.29@gmail.com>
2025-03-16 22:19:06 +0200

    module random

--- main.py
+++ main.py
@@ -1,8 +1,23 @@
 import turtle
+import random
 
+kitty = 50
+kitty += 80
+
+turtle.speed(200)
 turtle.color("green")
 turtle.shape("turtle")
 
-turtle.forward(100)
+game = True
 
-turtle.mainloop()
+idk = random.randint(0, 600)
+
+while range(12):
+    for i in range(idk):
+        turtle.left(kitty)
+        turtle.forward(kitty)
+        turtle.left(kitty)
+
+    
+
+turtle.done()
