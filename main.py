11d63e4d495aefe33605c576363f72dad2586771
Dell <misha.shulgin.29@gmail.com>
2025-03-17 23:00:07 +0200

    update

--- main.py
+++ main.py
@@ -10,13 +10,15 @@ turtle.shape("turtle")
 
 game = True
 
-idk = random.randint(0, 600)
-
-while range(12):
-    for i in range(idk):
-        turtle.left(kitty)
-        turtle.forward(kitty)
-        turtle.left(kitty)
+idk = random.randint(0, 300)
+if game == True:
+    while range(12):
+        for i in range(300):
+            turtle.left(idk)
+            turtle.forward(idk)
+            turtle.left(idk)
+            turtle.forward(idk)
+            turtle.right(idk)
 
     
 
